c26e04e09442aaf9ea9e7b636a0398eca86734ad
QuachNguyenChiHung <bacon3632@gmail.com>
2026-06-06 22:14:14 +0700

    Update index.ts

--- Exe-Frontend/store/index.ts
+++ Exe-Frontend/store/index.ts
@@ -14,7 +14,7 @@ export const store = configureStore({
                 ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
             },
         }),
-    devTools: process.env.NODE_ENV !== 'production',
+    devTools: import.meta.env.NODE_ENV !== 'production',
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
